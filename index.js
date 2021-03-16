54c54ec6990edf666f887657e5981ecc1589b3ba
Chris Dailey <nitz@users.noreply.github.com>
2021-03-16 11:06:33 -0400

    Update index.js

--- index.js
+++ index.js
@@ -108,7 +108,9 @@ class Action {
                 res.setEncoding("utf8")
                 res.on("data", chunk => body += chunk)
                 res.on("end", () => {
+                    console.log(`Existing versions JSON: ${body}`)
                     const existingVersions = JSON.parse(body)
+                    console.log(`Existing versions: ${existingVersions}`)
                     if (existingVersions.versions.indexOf(this.version) < 0)
                         this._pushPackage(this.version, this.packageName)
                 })
